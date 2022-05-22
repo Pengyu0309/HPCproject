b3d5c49092f33b57fb3be2e810c70a418d428678
Pengyu0309 <lipengyu0309@outlook.com>
2022-05-22 22:32:08 +0800

    use petsc to set the initial condition, generate the matrix A

--- main.c
+++ main.c
@@ -1,45 +1,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mkl.h>
+#include "petscksp.h"
+#include <petscvec.h>
+#include "petscmat.h"
 
 #define FLOAT_TYPE double
 
-int main(){
-	int i,j;
+int main(int argc, char **argv){
+	MPI_Comm        comm;
+        Vec             x, t;
+        Mat             A,U;
+	Petscint        i=0,j=0,N=10,n=10;
+        PetscReal       L = 1.0,t = 1.0,density = 1.0, c = 1.0,f=1.0,k=1.0,h=1.0,g=10;
 
-	double L = 1.0;
-	double density = 1.0;
-	double c =1;//heat capacity,J/kg-k
-	double f=1;//heat supply per unit volume
-	double k=1;//conductivity, W/m-k
-	double alpha;
+	// initialization
+	PetscInitialize(&argc, &argv, (char*)0, help);
+        comm = PETSC_COMM_WORLD;
 
+	// get option
+	PetscOptionsGetInt(PETSC_NULL, PETSC_NULL, "-space_num", &N, PETSC_NULL);
+	PetscOptionsGetInt(PETSC_NULL, PETSC_NULL, "-time_num", &n, PETSC_NULL);
+	PetscOptionsGetInt(PETSC_NULL, PETSC_NULL, "-num1", &i, PETSC_NULL);
+        PetscOptionsGetInt(PETSC_NULL, PETSC_NULL, "-num2", &j, PETSC_NULL);
+	PetscOptionsGetReal(PETSC_NULL, PETSC_NULL, "-length", &L, PETSC_NULL);
+        PetscOptionsGetReal(PETSC_NULL, PETSC_NULL, "-time", &t, PETSC_NULL);
+        PetscOptionsGetReal(PETSC_NULL, PETSC_NULL, "-density", &density, PETSC_NULL);
+	PetscOptionsGetReal(PETSC_NULL, PETSC_NULL, "-heat_capacity", &c, PETSC_NULL);//heat capacity,J/kg-k
+	PetscOptionsGetReal(PETSC_NULL, PETSC_NULL, "-heat_supply", &f, PETSC_NULL);//heat supply per unit volume
+	PetscOptionsGetReal(PETSC_NULL, PETSC_NULL, "-conductivity", &k, PETSC_NULL);//conductivity, W/m-k
+	PetscOptionsGetReal(PETSC_NULL, PETSC_NULL, "-heat flux", &h, PETSC_NULL);
+	PetscOptionsGetReal(PETSC_NULL, PETSC_NULL, "-boundary_temperature", &g, PETSC_NULL);//temperature
 	
-	//grid
-	int N = 10;
-        double dl;
-	FLOAT_TYPE *x;
-	x = (FLOAT_TYPE*) malloc(sizeof(double)*(N+1));
-	dl = L/N;
-	for (i=0;i<N+1;i++){
-		x[i]=i*dl;
+	//generate vector	
+	//space
+	VecCreate(comm, &x);
+        VecSetSizes(x, PETSC_DECIDE, L/N+1);
+        VecSetFromOptions(x);
+	for (i<(N+1);i++){
+		VecSetValue(x, i, i*(L/N), INSERT_VALUES);
 	}
+
+	//time
+	VecCreate(comm, &t);
+        VecSetSizes(t, PETSC_DECIDE, t/n+1);
+        VecSetFromOptions(t);
+        for (i<(n+1);i++){
+                VecSetValue(t, i, i*(t/n), INSERT_VALUES);
+        }
+
+	//generate matrix A
+	MatCreate(comm, &A);
+        MatSetSizes(A, PETSC_DECIDE, PETSC_DECIDE, L/N+1, L/N+1);
+        MatSetFromOptions(A);
+        MatMPIAIJSetPreallocation(A, 3, NULL, 3, NULL);
+        MatSeqAIJSetPreallocation(A, 3, NULL);
+
+        PetscInt        rstart, rend, M, m;
+        MatGetOwnershipRange(A, &rstart, &rend);
+        MatGetSize(A, &M, &m);
+        for (PetscInt i=rstart; i<rend; i++) {
+                PetscInt    index[3] = {i-1, i, i+1};
+                PetscScalar value[3] = {-1.0, 2.0, -1.0};
+                if (i == 0) {
+                        MatSetValues(A, 1, &i, 2, &index[1], &value[1], INSERT_VALUES);
+                }
+                else if (i == m-1) {
+                        MatSetValues(A, 1, &i, 2, index, value, INSERT_VALUES);
+                }
+                else {
+                        MatSetValues(A, 1, &i, 3, index, value, INSERT_VALUES);
+                }
+        }
+	MatSetValue(A,0,0,1,INSERT_VALUES);
+	MatSetValue(A,m-1,m-2,0,INSERT_VALUES);
+	MatSetValue(A,m-1,m-1,1,INSERT_VALUES);
+
+        MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
+        MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
+
+//      MatView(A, PETSC_VIEWER_STDOUT_WORLD);
+
+        MatSetOption(A, MAT_SYMMETRIC, PETSC_TRUE);
+
+
+
+
+
+
+
+
 
 //print the values of x	
 //	printf("x[%d] is: \n",N+1);
 //		for(i=0;i<N+1;i++){
 //			printf("%f\t",x[i]);}
-
-	//time
-	double time = 1;
-	double dt=0.1;
-	int M;
-	M=(time/dt)+1;
-	FLOAT_TYPE *t;
-        t = (FLOAT_TYPE*) malloc(sizeof(double)*M);
-	for (i=0; i<M;i++){
-		t[i]=i*dt;
-	}
 
 //print the values of t
 //	printf("t[%d] is: \n",M);
